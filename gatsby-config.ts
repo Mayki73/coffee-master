ae79b82d1a67b6e243caaf5788a71cd570a4f24b
Anton Simanenkau <antoniestories@gmail.com>
2023-07-24 14:22:07 +0300

    added logo in google

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `coffeeremont24`,
+    description: 'Ремонт кофемашин и кофеварок в Минске',
     siteUrl: `https://www.coffeeremont24.by`,
+    image: 'src/images/brands/icon-coffeeremont.png',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
